5d1ca3557b1fc0148afa45098fb27573bcbe2236
XinghaiGe <g18845381272@163.com>
2025-06-24 11:19:31 +0800

    feat: finish ch1

--- part1/ch1-linux-shell/test.sh
+++ part1/ch1-linux-shell/test.sh
@@ -1,1 +1,18 @@
+# runlevel
+# 0 shutdown system
+# 1 single user, system maintenance
+# 2 multiple user, no internet
+# 3 multiple user, internet
+# 4 custom
+# 5 multiple user, GUI
+# 6 reboot system
 runlevel
+
+# 查看默认目标
+systemctl get-default
+
+# Bourne Shell -> Bourne Again Shell -> Bash Shell
+
+# GNU's Not Unix
+
+# X Windows -> KDE Plasma, GNOME, ...
